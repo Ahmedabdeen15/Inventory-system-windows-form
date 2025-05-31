6479da330c1dc6149a19a46c4315ccf8a717df21
Ahmed Waleed <ahmedwalleed15@gmail.com>
2025-05-31 20:17:51 +0300

    Add employee_update form and associated logic for updating employee records

---
+++ Inventory/veiw/employee_update.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Inventory.veiw
+{
+    partial class employee_update
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 17;
+            label1.Text = "Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 95);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(367, 23);
+            textBox1.TabIndex = 16;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button2.ForeColor = SystemColors.Control;
+            button2.Location = new Point(259, 155);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 23);
+            button2.TabIndex = 15;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ControlLight;
+            button1.Location = new Point(12, 155);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 23);
+            button1.TabIndex = 14;
+            button1.Text = "update";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(11, 34);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(219, 23);
+            comboBox1.TabIndex = 18;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 15);
+            label2.TabIndex = 19;
+            label2.Text = "Choose employee";
+            // 
+            // employee_update
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(392, 200);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "employee_update";
+            Text = "employee_update";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button1;
+        private ComboBox comboBox1;
+        private Label label2;
+    }
+}
---
+++ Inventory/veiw/employee_update.cs
@@ -0,0 +1,70 @@
+﻿using Inventory.controller;
+using Inventory.data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory.veiw
+{
+    public partial class employee_update : Form
+    {
+        EmployeeControl employeeControl;
+        InventoryDbContext inventoryDb;
+        internal employee_update(InventoryDbContext inventoryDb)
+        {
+            InitializeComponent();
+            this.inventoryDb = inventoryDb;
+            employeeControl = new EmployeeControl(inventoryDb);
+            updateComboBox();
+        }
+
+        private void updateComboBox()
+        {
+            comboBox1.DataSource = employeeControl.getAllEmployees();
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "EmpId";
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                textBox1.Text = ((Employee)comboBox1.SelectedItem).Name;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("Please select an employee to update.");
+                    return;
+                }
+                int empId = ((Employee)comboBox1.SelectedItem).EmpId;
+                string name = textBox1.Text.Trim();
+                employeeControl.updateEmployee(empId, name);
+                MessageBox.Show("Employee updated successfully.");
+                updateComboBox();
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
