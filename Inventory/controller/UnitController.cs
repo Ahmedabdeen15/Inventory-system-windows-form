34b594e854ea5beddecda20fb6de3b9b57a8416e
Ahmed Waleed <ahmedwalleed15@gmail.com>
2025-06-01 17:52:49 +0300

    Add UnitController with methods for unit management: insert, update, and retrieve all units

---
+++ Inventory/controller/UnitController.cs
@@ -0,0 +1,48 @@
+﻿using Inventory.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.controller
+{
+    internal class UnitController
+    {
+        InventoryDbContext dbContext;
+        public UnitController(InventoryDbContext dbContext) { 
+            this.dbContext = dbContext;
+        }
+
+        public void insertUnit(String name) { 
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name can't be empty");
+            }
+            dbContext.units.Add(
+                new Unit
+                {
+                    Name = name
+                });
+            dbContext.SaveChanges();
+        }
+
+        public void updateUnit(int id,String name)
+        {
+            if (string.IsNullOrEmpty(name) | id < 0 )
+            {
+                throw new ArgumentNullException("name can't be empty");
+            }
+            Unit unit = dbContext.units.Find(id);
+            if (unit == null) {
+                throw new KeyNotFoundException("Item not found");
+            }
+            unit.Name = name;
+            dbContext.SaveChanges();
+        }
+        public List<Unit> getAllUnits()
+        {
+            return dbContext.units.ToList();
+        }
+    }
+}
