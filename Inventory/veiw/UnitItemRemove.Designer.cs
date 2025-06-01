91ae1040aa070d4e187988aaa4a65bec64e8a366
Ahmed Waleed <ahmedwalleed15@gmail.com>
2025-06-01 17:55:56 +0300

    Add UnitItemRemove form for unit management with item and unit selection

---
+++ Inventory/veiw/UnitItemRemove.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Inventory.veiw
+{
+    partial class UnitItemRemove
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button2 = new Button();
+            button1 = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            comboBox2 = new ComboBox();
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button2.ForeColor = SystemColors.Control;
+            button2.Location = new Point(12, 136);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 23);
+            button2.TabIndex = 32;
+            button2.Text = "Remove";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ControlLight;
+            button1.Location = new Point(259, 136);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 23);
+            button1.TabIndex = 31;
+            button1.Text = "Cancel";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 67);
+            label2.Name = "label2";
+            label2.Size = new Size(29, 15);
+            label2.TabIndex = 30;
+            label2.Text = "Unit";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 29;
+            label1.Text = "Item";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(12, 85);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(367, 23);
+            comboBox2.TabIndex = 28;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 27);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(367, 23);
+            comboBox1.TabIndex = 27;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // UnitItemRemove
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(392, 182);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Name = "UnitItemRemove";
+            Text = "UnitItemRemove";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button2;
+        private Button button1;
+        private Label label2;
+        private Label label1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+    }
+}
