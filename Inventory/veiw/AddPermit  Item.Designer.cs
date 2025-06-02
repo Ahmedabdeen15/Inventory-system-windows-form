862fa44682e6720e56a7c945efe2d6531fc119cd
Ahmed Waleed <ahmedwalleed15@gmail.com>
2025-06-02 21:20:38 +0300

    Add AddPermitItem form and associated resources for item management

---
+++ Inventory/veiw/AddPermit  Item.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Inventory.veiw
+{
+    partial class AddPermitItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label2 = new Label();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            comboBox2 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(8, 69);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 15);
+            label2.TabIndex = 20;
+            label2.Text = "quntity";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 19;
+            label1.Text = "Item";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(8, 257);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(367, 23);
+            textBox2.TabIndex = 18;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(11, 87);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(367, 23);
+            textBox1.TabIndex = 17;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button2.ForeColor = SystemColors.Control;
+            button2.Location = new Point(259, 302);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 23);
+            button2.TabIndex = 16;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ControlLight;
+            button1.Location = new Point(12, 302);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 23);
+            button1.TabIndex = 15;
+            button1.Text = "Insert";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(11, 27);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(364, 23);
+            comboBox1.TabIndex = 21;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(29, 15);
+            label3.TabIndex = 22;
+            label3.Text = "Unit";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(11, 141);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(364, 23);
+            comboBox2.TabIndex = 23;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Checked = false;
+            dateTimePicker1.Location = new Point(11, 196);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(364, 23);
+            dateTimePicker1.TabIndex = 24;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(11, 178);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 15);
+            label4.TabIndex = 25;
+            label4.Text = "pruduction date";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 239);
+            label5.Name = "label5";
+            label5.Size = new Size(86, 15);
+            label5.TabIndex = 26;
+            label5.Text = "Expiration days";
+            // 
+            // AddPermitItem
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(394, 344);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox2);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "AddPermitItem";
+            Text = "Add Item";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button1;
+        private ComboBox comboBox1;
+        private Label label3;
+        private ComboBox comboBox2;
+        private DateTimePicker dateTimePicker1;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ Inventory/veiw/AddPermit  Item.cs
@@ -0,0 +1,99 @@
+﻿using Inventory.controller;
+using Inventory.data;
+
+namespace Inventory.veiw
+{
+    internal partial class AddPermitItem : Form
+    {
+        InventoryDbContext InventoryDbContext;
+        ItemController itemController;
+        SupplyPermitController supplyPermitController;
+        int UpdatedItemId = -1;
+        public AddPermitItem(InventoryDbContext InventoryDbContext, SupplyPermitController supplyPermitController)
+        {
+            InitializeComponent();
+            this.InventoryDbContext = InventoryDbContext;
+            itemController = new ItemController(InventoryDbContext);
+            updateComboBox1();
+            this.supplyPermitController = supplyPermitController;
+        }
+        public AddPermitItem(InventoryDbContext InventoryDbContext, SupplyPermitController supplyPermitController, int id)
+        {
+            InitializeComponent();
+            this.InventoryDbContext = InventoryDbContext;
+            itemController = new ItemController(InventoryDbContext);
+            this.supplyPermitController = supplyPermitController;
+            updateComboBox1();
+            comboBox1.SelectedItem = supplyPermitController.suppliedItems[id].Item;
+            comboBox2.SelectedItem = supplyPermitController.suppliedItems[id].Unit;
+            textBox1.Text = supplyPermitController.suppliedItems[id].quntity.ToString();
+            textBox2.Text = supplyPermitController.suppliedItems[id].ExpirationDays.ToString();
+            dateTimePicker1.Value = supplyPermitController.suppliedItems[id].ProudctionDate;
+            UpdatedItemId = id;
+            this.Text = "Update Item";
+            button1.Text = "Update";
+        }
+
+        private void updateComboBox1()
+        {
+            comboBox1.DataSource = itemController.getAllItems();
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "ItemId";
+            updateComboBox2(1);
+        }
+        private void updateComboBox2(int id)
+        {
+            comboBox2.DataSource = itemController.getItemUnitsById(id);
+            comboBox2.DisplayMember = "Name";
+            comboBox2.ValueMember = "UnitId";
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int ItemId = ((Item)comboBox1.SelectedItem).ItemId;
+            updateComboBox2(ItemId);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedItem == null || comboBox2.SelectedItem == null)
+                {
+                    MessageBox.Show("Please select an item to update.");
+                    return;
+                }
+                else
+                {
+                    float quntity;
+                    int expDay;
+                    if (!(float.TryParse(textBox1.Text, out quntity) && int.TryParse(textBox2.Text, out expDay)))
+                    {
+                        MessageBox.Show("numeric input error");
+                        return;
+                    }
+                    else
+                    {
+                        Item item = (Item)comboBox1.SelectedItem;
+                        Unit unit = (Unit)comboBox2.SelectedItem;
+                        if (UpdatedItemId == -1)
+                            supplyPermitController.addSupplyItemPermit(quntity, dateTimePicker1.Value.Date, expDay, item, unit);
+                        else
+                            supplyPermitController.updateSupplyItemPermit(UpdatedItemId, quntity, dateTimePicker1.Value.Date, expDay, item, unit);
+                        this.DialogResult = DialogResult.OK;
+                        this.Close();
+                    }
+
+                }
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
