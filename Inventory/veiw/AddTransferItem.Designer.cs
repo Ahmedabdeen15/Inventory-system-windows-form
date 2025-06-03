c0f4189e0de258876cbbb27dd4071a7c554d2e05
Ahmed Abdeen <ahmedwalleed15@gmail.com>
2025-06-03 16:35:50 +0300

    Refactor AddTransferItem form to enhance data loading and item selection functionality

--- Inventory/veiw/AddTransferItem.Designer.cs
+++ Inventory/veiw/AddTransferItem.Designer.cs
@@ -47,88 +47,91 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(145, 53);
+            label7.Location = new Point(166, 71);
             label7.Name = "label7";
-            label7.Size = new Size(38, 15);
+            label7.Size = new Size(50, 20);
             label7.TabIndex = 51;
             label7.Text = "label7";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(13, 53);
+            label8.Location = new Point(15, 71);
             label8.Name = "label8";
-            label8.Size = new Size(37, 15);
+            label8.Size = new Size(31, 20);
             label8.TabIndex = 50;
-            label8.Text = "Item :";
+            label8.Text = "ID :";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(145, 158);
+            label6.Location = new Point(166, 211);
             label6.Name = "label6";
-            label6.Size = new Size(38, 15);
+            label6.Size = new Size(50, 20);
             label6.TabIndex = 49;
             label6.Text = "label6";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(145, 123);
+            label5.Location = new Point(166, 164);
             label5.Name = "label5";
-            label5.Size = new Size(38, 15);
+            label5.Size = new Size(50, 20);
             label5.TabIndex = 48;
             label5.Text = "label5";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 158);
+            label4.Location = new Point(14, 211);
             label4.Name = "label4";
-            label4.Size = new Size(95, 15);
+            label4.Size = new Size(118, 20);
             label4.TabIndex = 47;
             label4.Text = "Production date:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(13, 123);
+            label3.Location = new Point(15, 164);
             label3.Name = "label3";
-            label3.Size = new Size(35, 15);
+            label3.Size = new Size(138, 20);
             label3.TabIndex = 46;
-            label3.Text = "Unit :";
+            label3.Text = "Quntity in storage : ";
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(12, 27);
+            comboBox1.Location = new Point(14, 36);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(364, 23);
+            comboBox1.Size = new Size(415, 28);
             comboBox1.TabIndex = 45;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(9, 73);
+            label2.Location = new Point(10, 97);
             label2.Name = "label2";
-            label2.Size = new Size(45, 15);
+            label2.Size = new Size(55, 20);
             label2.TabIndex = 44;
             label2.Text = "quntity";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(14, 12);
             label1.Name = "label1";
-            label1.Size = new Size(86, 15);
+            label1.Size = new Size(90, 20);
             label1.TabIndex = 43;
-            label1.Text = "choose Item ID";
+            label1.Text = "choose Item";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 91);
+            textBox1.Location = new Point(14, 121);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(367, 23);
+            textBox1.Size = new Size(419, 27);
             textBox1.TabIndex = 42;
             // 
             // button2
@@ -137,12 +140,14 @@
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
             button2.ForeColor = SystemColors.Control;
-            button2.Location = new Point(260, 221);
+            button2.Location = new Point(297, 295);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(120, 23);
+            button2.Size = new Size(137, 31);
             button2.TabIndex = 41;
             button2.Text = "Cancel";
             button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // button1
             // 
@@ -150,36 +155,38 @@
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
             button1.ForeColor = SystemColors.ControlLight;
-            button1.Location = new Point(13, 221);
+            button1.Location = new Point(15, 295);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(120, 23);
+            button1.Size = new Size(137, 31);
             button1.TabIndex = 40;
             button1.Text = "Insert";
             button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(146, 190);
+            label9.Location = new Point(167, 253);
             label9.Name = "label9";
-            label9.Size = new Size(38, 15);
+            label9.Size = new Size(50, 20);
             label9.TabIndex = 53;
             label9.Text = "label9";
             // 
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(13, 190);
+            label10.Location = new Point(15, 253);
             label10.Name = "label10";
-            label10.Size = new Size(93, 15);
+            label10.Size = new Size(118, 20);
             label10.TabIndex = 52;
             label10.Text = "Exptitation days:";
             // 
             // AddTransferItem
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(394, 258);
+            ClientSize = new Size(450, 344);
             Controls.Add(label9);
             Controls.Add(label10);
             Controls.Add(label7);
@@ -194,6 +201,7 @@
             Controls.Add(textBox1);
             Controls.Add(button2);
             Controls.Add(button1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "AddTransferItem";
             Text = "AddTransferItem";
             ResumeLayout(false);
--- Inventory/veiw/AddTransferItem.cs
+++ Inventory/veiw/AddTransferItem.cs
@@ -7,14 +7,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Inventory.controller;
+using Inventory.data;
+using Microsoft.EntityFrameworkCore;
 namespace Inventory.veiw
 {
-    public partial class AddTransferItem : Form
+    internal partial class AddTransferItem : Form
     {
-        public AddTransferItem()
+        InventoryDbContext dbContext;
+        TransferController transferController;
+        List<WarehouseItem> warehouseItems;
+        public AddTransferItem(InventoryDbContext dbContext, TransferController transferController, List<WarehouseItem> warehouseItems)
         {
             InitializeComponent();
+            this.dbContext = dbContext;
+            this.transferController = transferController;
+            this.warehouseItems = warehouseItems;
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            var temp = (from item in warehouseItems
+                        select new
+                        {
+                            Id = item.Id,
+                            name = item.Item.Name,
+                            pDate = item.productionDate.ToString("yyyy-MM-dd"),
+                            eDate = item.ExpirationDate.ToString("yyyy-MM-dd"),
+                            quntity = item.quntity.ToString() + " " + item.Unit.Name,
+                            item = item.Item,
+                            Warehouse = item.Warehouse,
+                            warehouseItem = item,
+                        }).ToList();
+            comboBox1.DataSource = temp;
+            comboBox1.DisplayMember = "name";
+            comboBox1.ValueMember = "Id";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label7.Text = ((dynamic)comboBox1.SelectedItem).Id.ToString();
+            label5.Text = ((dynamic)comboBox1.SelectedItem).quntity;
+            label6.Text = ((dynamic)comboBox1.SelectedItem).pDate;
+            label9.Text = ((dynamic)comboBox1.SelectedItem).eDate;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("Please select an item to update.");
+                    return;
+                }
+                else
+                {
+                    float quntity;
+                    if (!(float.TryParse(textBox1.Text, out quntity)))
+                    {
+                        MessageBox.Show("numeric input error");
+                        return;
+                    }
+                    else
+                    {
+                        WarehouseItem item = ((dynamic)comboBox1.SelectedItem).warehouseItem;
+                        transferController.addTransferedItem(item, quntity);
+                        this.DialogResult = DialogResult.OK;
+                        this.Close();
+                    }
+
+                }
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
         }
     }
 }
