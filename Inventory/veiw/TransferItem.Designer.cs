84979f38c7e03064fb3a175c6ff4b3b78016e30e
Ahmed Waleed <ahmedwalleed15@gmail.com>
2025-06-03 07:54:39 +0300

    Add TransferItem form and associated resources for managing item transfers

---
+++ Inventory/veiw/TransferItem.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Inventory.veiw
+{
+    partial class TransferItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label6 = new Label();
+            comboBox3 = new ComboBox();
+            label5 = new Label();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 9);
+            label6.Name = "label6";
+            label6.Size = new Size(154, 15);
+            label6.TabIndex = 59;
+            label6.Text = "Warehouse to transfer From";
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(12, 28);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(246, 23);
+            comboBox3.TabIndex = 58;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(689, 3);
+            label5.Name = "label5";
+            label5.Size = new Size(74, 15);
+            label5.TabIndex = 57;
+            label5.Text = "Added Items";
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.SpringGreen;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(12, 147);
+            button3.Name = "button3";
+            button3.Size = new Size(246, 23);
+            button3.TabIndex = 56;
+            button3.Text = "Add Item";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button2.ForeColor = SystemColors.Control;
+            button2.Location = new Point(819, 376);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 23);
+            button2.TabIndex = 55;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ControlLight;
+            button1.Location = new Point(47, 376);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 23);
+            button1.TabIndex = 54;
+            button1.Text = "Transfer";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(288, 21);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(674, 349);
+            dataGridView1.TabIndex = 53;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 86);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(246, 23);
+            comboBox1.TabIndex = 51;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(139, 15);
+            label2.TabIndex = 47;
+            label2.Text = "Warehouse to transfer To";
+            // 
+            // TransferItem
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(982, 411);
+            Controls.Add(label6);
+            Controls.Add(comboBox3);
+            Controls.Add(label5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Name = "TransferItem";
+            Text = "TransferItem";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label6;
+        private ComboBox comboBox3;
+        private Label label5;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private ComboBox comboBox1;
+        private Label label2;
+    }
+}
