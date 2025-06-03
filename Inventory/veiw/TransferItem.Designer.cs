8b1cf8dc72be6f9c714ebd4c70620b62a31367c8
Ahmed Abdeen <ahmedwalleed15@gmail.com>
2025-06-03 16:35:29 +0300

    Enhance TransferItem form with additional controls and event handlers for improved item transfer management

--- Inventory/veiw/TransferItem.Designer.cs
+++ Inventory/veiw/TransferItem.Designer.cs
@@ -37,32 +37,36 @@
             dataGridView1 = new DataGridView();
             comboBox1 = new ComboBox();
             label2 = new Label();
+            comboBox2 = new ComboBox();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(12, 9);
+            label6.Location = new Point(14, 12);
             label6.Name = "label6";
-            label6.Size = new Size(154, 15);
+            label6.Size = new Size(192, 20);
             label6.TabIndex = 59;
             label6.Text = "Warehouse to transfer From";
             // 
             // comboBox3
             // 
             comboBox3.FormattingEnabled = true;
-            comboBox3.Location = new Point(12, 28);
+            comboBox3.Location = new Point(14, 37);
+            comboBox3.Margin = new Padding(3, 4, 3, 4);
             comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(246, 23);
+            comboBox3.Size = new Size(281, 28);
             comboBox3.TabIndex = 58;
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(689, 3);
+            label5.Location = new Point(787, 4);
             label5.Name = "label5";
-            label5.Size = new Size(74, 15);
+            label5.Size = new Size(94, 20);
             label5.TabIndex = 57;
             label5.Text = "Added Items";
             // 
@@ -72,12 +76,14 @@
             button3.FlatStyle = FlatStyle.Flat;
             button3.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
             button3.ForeColor = SystemColors.ActiveCaptionText;
-            button3.Location = new Point(12, 147);
+            button3.Location = new Point(14, 271);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(246, 23);
+            button3.Size = new Size(281, 31);
             button3.TabIndex = 56;
             button3.Text = "Add Item";
             button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
             // 
             // button2
             // 
@@ -85,12 +91,14 @@
             button2.FlatStyle = FlatStyle.Flat;
             button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
             button2.ForeColor = SystemColors.Control;
-            button2.Location = new Point(819, 376);
+            button2.Location = new Point(936, 501);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(120, 23);
+            button2.Size = new Size(137, 31);
             button2.TabIndex = 55;
             button2.Text = "Cancel";
             button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // button1
             // 
@@ -98,44 +106,68 @@
             button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
             button1.ForeColor = SystemColors.ControlLight;
-            button1.Location = new Point(47, 376);
+            button1.Location = new Point(54, 501);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(120, 23);
+            button1.Size = new Size(137, 31);
             button1.TabIndex = 54;
             button1.Text = "Transfer";
             button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // dataGridView1
             // 
             dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(288, 21);
+            dataGridView1.Location = new Point(329, 28);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(674, 349);
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(770, 465);
             dataGridView1.TabIndex = 53;
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(12, 86);
+            comboBox1.Location = new Point(14, 115);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(246, 23);
+            comboBox1.Size = new Size(281, 28);
             comboBox1.TabIndex = 51;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 68);
+            label2.Location = new Point(14, 91);
             label2.Name = "label2";
-            label2.Size = new Size(139, 15);
+            label2.Size = new Size(174, 20);
             label2.TabIndex = 47;
             label2.Text = "Warehouse to transfer To";
             // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(14, 197);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(281, 28);
+            comboBox2.TabIndex = 60;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(14, 165);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 20);
+            label1.TabIndex = 61;
+            label1.Text = "Supplier";
+            // 
             // TransferItem
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(982, 411);
+            ClientSize = new Size(1122, 548);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
             Controls.Add(label6);
             Controls.Add(comboBox3);
             Controls.Add(label5);
@@ -145,6 +177,7 @@
             Controls.Add(dataGridView1);
             Controls.Add(comboBox1);
             Controls.Add(label2);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "TransferItem";
             Text = "TransferItem";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
@@ -162,5 +195,7 @@
         private DataGridView dataGridView1;
         private ComboBox comboBox1;
         private Label label2;
+        private ComboBox comboBox2;
+        private Label label1;
     }
 }
--- Inventory/veiw/TransferItem.cs
+++ Inventory/veiw/TransferItem.cs
@@ -7,14 +7,266 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Inventory.controller;
+using Inventory.data;
+using Microsoft.EntityFrameworkCore;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Inventory.veiw
 {
-    public partial class TransferItem : Form
+    internal partial class TransferItem : Form
     {
-        public TransferItem()
+        InventoryDbContext dbContext;
+        TransferController transferController;
+        int hoveredRowIndex = -1;
+        int hoveredColumnIndex = -1;
+        public TransferItem(InventoryDbContext InventoryDbContext)
         {
             InitializeComponent();
+            this.dbContext = InventoryDbContext;
+            transferController = new TransferController(InventoryDbContext);
+            LoadWarehouses();
+            dataGridView1.CellPainting += dataGridView1_CellPainting;
+            dataGridView1.CellMouseEnter += dataGridView1_CellMouseEnter;
+            dataGridView1.CellMouseLeave += dataGridView1_CellMouseLeave;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+
+        }
+        private void LoadWarehouses()
+        {
+            // Example method to load warehouses into a combo box
+            var warehouses = dbContext.warehouses.Include(w => w.warehouseItems)
+                .ThenInclude(i => i.Item)
+                .Include(w => w.warehouseItems)
+                .ThenInclude(i => i.Unit)
+                .Include(w => w.supplyPermits)
+                .ThenInclude(sp => sp.Supplier)
+                .Include(w => w.supplyPermits)
+                .ThenInclude(sp => sp.suppliedItems)
+                .ToList();
+            var warehouses1 = warehouses.ToList();
+            var warehouses2 = warehouses.ToList();
+
+            comboBox1.DataSource = warehouses1;
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "WarehouseID";
+            comboBox3.DataSource = warehouses2;
+            comboBox3.DisplayMember = "Name";
+            comboBox3.ValueMember = "WarehouseID";
+        }
+        private void dataGridView1_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                string colName = dataGridView1.Columns[e.ColumnIndex].Name;
+                if (colName == "EditButton" || colName == "RemoveButton")
+                {
+                    hoveredRowIndex = e.RowIndex;
+                    hoveredColumnIndex = e.ColumnIndex;
+                    dataGridView1.InvalidateCell(e.ColumnIndex, e.RowIndex);
+                }
+            }
+        }
+
+        private void dataGridView1_CellMouseLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                string colName = dataGridView1.Columns[e.ColumnIndex].Name;
+                if (colName == "EditButton" || colName == "RemoveButton")
+                {
+                    hoveredRowIndex = -1;
+                    hoveredColumnIndex = -1;
+                    dataGridView1.InvalidateCell(e.ColumnIndex, e.RowIndex);
+                }
+            }
+        }
+
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            if (e.ColumnIndex >= 0 && dataGridView1.Columns[e.ColumnIndex].Name == "EditButton" && e.RowIndex >= 0)
+            {
+                e.PaintBackground(e.CellBounds, true);
+                Color backColor = (e.RowIndex == hoveredRowIndex) ? Color.Orange : Color.Gold;
+
+                // Fill background
+                using (Brush backgroundBrush = new SolidBrush(backColor))
+                {
+                    e.Graphics.FillRectangle(backgroundBrush, e.CellBounds);
+                }
+
+                // Draw border
+                using (Pen pen = new Pen(Color.Black))
+                {
+                    e.Graphics.DrawRectangle(pen, e.CellBounds.X, e.CellBounds.Y, e.CellBounds.Width - 1, e.CellBounds.Height - 1);
+                }
+
+                // Draw centered text
+                string buttonText = "Edit";
+                using (Font font = new Font("Segoe UI Black", 9F, FontStyle.Bold))
+                using (Brush textBrush = new SolidBrush(Color.Black))
+                {
+                    SizeF textSize = e.Graphics.MeasureString(buttonText, font);
+                    float textX = e.CellBounds.X + (e.CellBounds.Width - textSize.Width) / 2;
+                    float textY = e.CellBounds.Y + (e.CellBounds.Height - textSize.Height) / 2;
+                    e.Graphics.DrawString(buttonText, font, textBrush, textX, textY);
+                }
+
+                e.Handled = true;
+            }
+            if (e.ColumnIndex >= 0 && dataGridView1.Columns[e.ColumnIndex].Name == "RemoveButton" && e.RowIndex >= 0)
+            {
+                e.PaintBackground(e.CellBounds, true);
+                Color backColor = (e.RowIndex == hoveredRowIndex) ? Color.LightCoral : Color.Tomato;
+
+                // Fill background
+                using (Brush backgroundBrush = new SolidBrush(backColor))
+                {
+                    e.Graphics.FillRectangle(backgroundBrush, e.CellBounds);
+                }
+                // Draw border
+                using (Pen pen = new Pen(Color.Black))
+                {
+                    e.Graphics.DrawRectangle(pen, e.CellBounds.X, e.CellBounds.Y, e.CellBounds.Width - 1, e.CellBounds.Height - 1);
+                }
+
+                // Draw centered text
+                string buttonText = "Remove";
+                using (Font font = new Font("Segoe UI Black", 9F, FontStyle.Bold))
+                using (Brush textBrush = new SolidBrush(Color.Black))
+                {
+                    SizeF textSize = e.Graphics.MeasureString(buttonText, font);
+                    float textX = e.CellBounds.X + (e.CellBounds.Width - textSize.Width) / 2;
+                    float textY = e.CellBounds.Y + (e.CellBounds.Height - textSize.Height) / 2;
+                    e.Graphics.DrawString(buttonText, font, textBrush, textX, textY);
+                }
+
+                e.Handled = true;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Warehouse warehouse = (Warehouse)comboBox3.SelectedItem;
+            var query = (from w in (warehouse).supplyPermits
+                        where w.Supplier.SupplierID == ((Supplier)comboBox2.SelectedItem).SupplierID
+                        from item in w.suppliedItems
+                        select item).ToList();
+            List<WarehouseItem> warehouseItems = (from SI in query select SI.WarehouseItem).ToList();
+            AddTransferItem addTransferItem = new AddTransferItem(dbContext, transferController, warehouseItems);
+            var confirmResult = addTransferItem.ShowDialog();
+            if (confirmResult == DialogResult.OK)
+            {
+                invalDataGrid();
+            }
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                string columnName = dataGridView1.Columns[e.ColumnIndex].Name;
+                //if (columnName == "EditButton")
+                //{
+                //    Warehouse warehouse = ((ReleasePermit)comboBox3.SelectedItem).Warehouse;
+                //    AddReleasePermitItem addPermitItem = new AddReleasePermitItem(dbContext, ReleasePermitController, e.RowIndex, warehouse);
+                //    var confirmResult = addPermitItem.ShowDialog();
+                //    if (confirmResult == DialogResult.OK)
+                //    {
+                //        invalDataGrid();
+                //    }
+                //}
+                //else
+                if (columnName == "RemoveButton")
+                {
+                    DialogResult result = MessageBox.Show(
+                        "Are you sure you want to delete this item?",
+                        "Confirm Deletion",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Warning
+                    );
+
+                    if (result == DialogResult.Yes)
+                    {
+                        transferController.transferItems.RemoveAt(e.RowIndex);
+                        invalDataGrid();
+                    }
+                }
+            }
+        }
+        private void invalDataGrid()
+        {
+            var tempData = transferController.getTreansferItems();
+            if (tempData != null && tempData.Count != 0)
+            {
+                dataGridView1.DataSource = tempData;
+                if (dataGridView1.Columns.Contains("ID"))
+                {
+                    dataGridView1.Columns["ID"].Visible = false;
+                }
+                //if (!dataGridView1.Columns.Contains("EditButton"))
+                //{
+                //    DataGridViewButtonColumn editButtonColumn = new DataGridViewButtonColumn();
+                //    editButtonColumn.Name = "EditButton";
+                //    editButtonColumn.HeaderText = "Edit";
+                //    editButtonColumn.Text = "Edit";
+                //    editButtonColumn.UseColumnTextForButtonValue = true;
+                //    dataGridView1.Columns.Add(editButtonColumn);
+                //}
+                if (!dataGridView1.Columns.Contains("RemoveButton"))
+                {
+                    DataGridViewButtonColumn editButtonColumn = new DataGridViewButtonColumn();
+                    editButtonColumn.Name = "RemoveButton";
+                    editButtonColumn.HeaderText = "Remove";
+                    editButtonColumn.Text = "Remove";
+                    editButtonColumn.UseColumnTextForButtonValue = true;
+                    dataGridView1.Columns.Add(editButtonColumn);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (transferController.transferItems.Count == 0)
+                {
+                    MessageBox.Show("Please add at least one item to the permit.");
+                    return;
+                }
+                Supplier supplier = (Supplier)comboBox2.SelectedItem;
+                Warehouse FromWarehouse = ((Warehouse)comboBox3.SelectedItem);
+                Warehouse ToWarehouse = ((Warehouse)comboBox1.SelectedItem);
+                if (supplier == null || ToWarehouse == null || FromWarehouse == null)
+                {
+                    MessageBox.Show("Please select a supplier and warehouses.");
+                    return;
+                }
+                transferController.saveTransferedItems(FromWarehouse , ToWarehouse, supplier);
+                MessageBox.Show("Permit transfer done successfully.");
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var suppliers = ((Warehouse)comboBox3.SelectedItem).supplyPermits
+                .Select(sp => sp.Supplier)
+                .DistinctBy(s => s.SupplierID)
+                .ToList();
+            comboBox2.DataSource = suppliers;
+            comboBox2.DisplayMember = "name";
+            comboBox2.ValueMember = "SupplierID";
         }
     }
 }
