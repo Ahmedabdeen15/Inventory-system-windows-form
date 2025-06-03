971623673416be7b322ea80e7aa25f03cb875ecd
Ahmed Waleed <ahmedwalleed15@gmail.com>
2025-06-03 07:42:25 +0300

    Add AddReleasePermitItem form and associated resources for managing release item permits

---
+++ Inventory/veiw/AddReleasePermitItem.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Inventory.veiw
+{
+    partial class AddReleasePermitItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(13, 123);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 15);
+            label3.TabIndex = 34;
+            label3.Text = "Unit :";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 27);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(364, 23);
+            comboBox1.TabIndex = 33;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(9, 69);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 15);
+            label2.TabIndex = 32;
+            label2.Text = "quntity";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 15);
+            label1.TabIndex = 31;
+            label1.Text = "choose Item ID";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 87);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(367, 23);
+            textBox1.TabIndex = 29;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button2.ForeColor = SystemColors.Control;
+            button2.Location = new Point(260, 192);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 23);
+            button2.TabIndex = 28;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ControlLight;
+            button1.Location = new Point(13, 192);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 23);
+            button1.TabIndex = 27;
+            button1.Text = "Insert";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 158);
+            label4.Name = "label4";
+            label4.Size = new Size(95, 15);
+            label4.TabIndex = 35;
+            label4.Text = "Production date:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(145, 123);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 36;
+            label5.Text = "label5";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(145, 158);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 37;
+            label6.Text = "label6";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(145, 53);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 15);
+            label7.TabIndex = 39;
+            label7.Text = "label7";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(13, 53);
+            label8.Name = "label8";
+            label8.Size = new Size(37, 15);
+            label8.TabIndex = 38;
+            label8.Text = "Item :";
+            // 
+            // AddReleasePermitItem
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(402, 241);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "AddReleasePermitItem";
+            Text = "AddReleasePermitItem";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button1;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+    }
+}
---
+++ Inventory/veiw/AddReleasePermitItem.cs
@@ -0,0 +1,109 @@
+﻿using Inventory.controller;
+using Inventory.data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory.veiw
+{
+    internal partial class AddReleasePermitItem : Form
+    {
+        InventoryDbContext InventoryDbContext;
+        ItemController itemController;
+        ReleaseItemController addReleasePermitItem;
+        Warehouse warehouse;
+        int releasedItemsId = -1;
+        public AddReleasePermitItem(InventoryDbContext InventoryDbContext, ReleaseItemController addReleasePermitItem,Warehouse warehouse)
+        {
+            InitializeComponent();
+            this.InventoryDbContext = InventoryDbContext;
+            itemController = new ItemController(InventoryDbContext);
+            this.addReleasePermitItem = addReleasePermitItem;
+            this.warehouse = warehouse;
+            updateComboBox1();
+
+        }
+        public AddReleasePermitItem(InventoryDbContext InventoryDbContext, ReleaseItemController addReleasePermitItem, int id, Warehouse warehouse)
+        {
+            InitializeComponent();
+            this.InventoryDbContext = InventoryDbContext;
+            itemController = new ItemController(InventoryDbContext);
+            this.warehouse = warehouse;
+            this.addReleasePermitItem = addReleasePermitItem;
+            comboBox1.SelectedItem = addReleasePermitItem.releasedItems[id].warehouseItem.Item;
+            label5.Text = addReleasePermitItem.releasedItems[id].warehouseItem.Unit.Name;
+            textBox1.Text = addReleasePermitItem.releasedItems[id].quntity.ToString();
+            releasedItemsId = id;
+            updateComboBox1();
+            this.Text = "Update Item";
+            button1.Text = "Update";
+        }
+
+        private void updateComboBox1()
+        {
+            var warehouseItems = InventoryDbContext.warehouseItems
+                .Where(wi => wi.Warehouse.WarehouseID == warehouse.WarehouseID)
+                .Include(wi => wi.Unit)
+                .Include(wi => wi.Item)
+                .ToList();
+            comboBox1.DataSource = warehouseItems;
+            comboBox1.DisplayMember = "Item.Name";
+            comboBox1.ValueMember = "Id";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label5.Text = ((WarehouseItem)comboBox1.SelectedItem).Unit.Name;
+            label6.Text = ((WarehouseItem)comboBox1.SelectedItem).productionDate.ToString();
+            label7.Text = ((WarehouseItem)comboBox1.SelectedItem).Item.Name;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedItem == null )
+                {
+                    MessageBox.Show("Please select an item to update.");
+                    return;
+                }
+                else
+                {
+                    float quntity;
+                    if (!(float.TryParse(textBox1.Text, out quntity)))
+                    {
+                        MessageBox.Show("numeric input error");
+                        return;
+                    }
+                    else
+                    {
+                        WarehouseItem item = (WarehouseItem)comboBox1.SelectedItem;
+                        if (releasedItemsId == -1)
+                            addReleasePermitItem.addReleaseItemPermit(quntity, item);
+                        else
+                            addReleasePermitItem.updateReleaseItemPermit(quntity, item , releasedItemsId);
+                        this.DialogResult = DialogResult.OK;
+                        this.Close();
+                    }
+
+                }
+            }
+            catch (Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
